390b0dd34d941ad8e56375998670a83c6542af40
JasonHippo <48232441+JasonHippo@users.noreply.github.com>
2021-12-27 22:15:53 +0800

    modify comments

--- deep-text-recognition-benchmark/processing/crop_AE_ch.py
+++ deep-text-recognition-benchmark/processing/crop_AE_ch.py
@@ -2,8 +2,8 @@ import cv2
 import json
 import glob
 
-count = 188701
-g = glob.glob("/nfs/Workspace/T-Brain/img/*jpg")
+count = 188701 # T-brain already crop 188701 words
+g = glob.glob("ReCTS_img/*jpg") # ReCTS folder
 g.sort()
 
 for path in g:
@@ -19,7 +19,7 @@ for path in g:
             x1 = p[0]; x2 = p[4]; y1 = p[1]; y2 = p[5]
             crop_img = img[y1:y2, x1:x2]
             crop_img = cv2.resize(crop_img, (64, 64), interpolation=cv2.INTER_CUBIC)
-            cv2.imwrite("/nfs/Workspace/deep-text-recognition-benchmark/data_tbrain_and_AE/train/test/"+"word_{}.png".format(count), crop_img)
-            with open("/nfs/Workspace/deep-text-recognition-benchmark/data_tbrain_and_AE/train/gt.txt", 'a', encoding="utf-8") as txt:
+            cv2.imwrite("data_tbrain_and_AE/train/test/"+"word_{}.png".format(count), crop_img)
+            with open("data_tbrain_and_AE/train/gt.txt", 'a', encoding="utf-8") as txt:
                 txt.write("test/word_{}.png {}\n".format(count, char["transcription"]))
     print("Done {}, now counts to {}.".format(path, count))
